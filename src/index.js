1a1bbd316ddfc2da86d884f9086a505b8eecae3f
chaya <cgw9219@gmail.com>
2023-04-18 15:04:04 +0300

    changes 18.04

--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class TodoApp extends React.Component {
-  render() {
-    const todoList = JSON.parse(localStorage.getItem('todoList'));
-    return (
-      <div>
-        {todoList.map((todo) => (
-          <p key={todo.id}>{todo.text}</p>
-        ))}
-      </div>
-    );
+function getIntialToDo() {
+  const todoList = JSON.parse(localStorage.getItem('todoList'));
+  if (todoList) {
+    return todoList;
+  } else {
+    const todoList = [
+      { id: 1, text: 'Todo 1', completed: false },
+      { id: 2, text: 'Todo 2', completed: true },
+      { id: 3, text: 'Todo 3', completed: false },
+    ];
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    return todoList;
   }
 }
 
-const todoList = [
-  { id: 1, text: 'Todo 1', completed: false },
-  { id: 2, text: 'Todo 2', completed: true },
-  { id: 3, text: 'Todo 3', completed: false },
-];
-localStorage.setItem('todoList', JSON.stringify(todoList));
+function TodoApp() {
+  const todoList = getIntialToDo();
+  return (
+    <div>
+      {todoList.map((todo) => (
+        <p key={todo.id}>{todo.text}</p>
+      ))}
+    </div>
+  );
+}
 
 ReactDOM.render(<TodoApp />, document.getElementById('app'));
